09a9193ab08e81d081af47fa3fdbcdc5d26cfab9
DanielaGC <danielagc676@gmail.com>
2019-09-19 21:20:11 -0300

    Adicionar outras informações no HelpCommand.js

--- commands/misc/HelpCommand.js
+++ commands/misc/HelpCommand.js
@@ -14,17 +14,22 @@ module.exports = class HelpCommand extends Command {
         let music = this.client.commands.filter(cmd => cmd.config.category === "music").map(cmd => `**${server.prefix}${cmd.config.name}** » ${t(`help:${cmd.config.name}`)}`).join("\n")
         let misc = this.client.commands.filter(cmd => cmd.config.category === "misc").map(cmd => `**${server.prefix}${cmd.config.name}** » ${t(`help:${cmd.config.name}`)}`).join("\n")
         let settings = this.client.commands.filter(cmd => cmd.config.category === "settings").map(cmd => `**${server.prefix}${cmd.config.name}** » ${t(`help:${cmd.config.name}`)}`).join("\n")
-
-        const embed = new MessageEmbed()
-        .setColor(this.client.colors.default)
-        .addField(t("commands:help.music"), music)
-        .addField(t("commands:help.misc"), misc)
-        .addField(t("commands:help.settings"), settings)
-
-        message.author.send(embed).then(() => {
-            message.channel.send(t("commands:dm.send-dm"))
-        }).catch(() => {
-            message.channel.send(t("commands:dm.dm-closed"))
+        let invite = this.client.generateInvite(37047552)
+        invite.then(i => {
+            let links = `[${t("commands:invite.title")}](${i}) - [${t("commands:support-server")}](https://discord.gg/c8EWvFK)\n[Discord Bot List](https://discordbots.org/bot/481289027753082890/vote) - [Bots Para Discord]https://botsparadiscord.xyz/bots/481289027753082890/votar)`
+            const embed = new MessageEmbed()
+            .setColor(this.client.colors.default)
+            .addField(t("commands:help.music"), music)
+            .addField(t("commands:help.misc"), misc)
+            .addField(t("commands:help.settings"), settings)
+            .addBlankField(true)
+            .addField(t("commands:help.another-links"), `**${links}**`)
+    
+            message.author.send(embed).then(() => {
+                message.channel.send(t("commands:dm.send-dm"))
+            }).catch(() => {
+                message.channel.send(t("commands:dm.dm-closed"))
+            })
         })
     }
 }
