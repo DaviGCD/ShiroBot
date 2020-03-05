754752b1fbc2e2f7db7069e5f704edc746b8cba6
DanielaGC <danielagc676@gmail.com>
2020-03-05 01:44:33 -0300

    Fixed ShiroClient

--- src/ShiroClient.js
+++ src/ShiroClient.js
@@ -17,12 +17,12 @@ module.exports = class ShiroClient extends Client {
     }
 
     loadCommands() {
-        readdir(__dirname, (err, f) => {
+        readdir(`${__dirname}/commands`, (err, f) => {
             if (err) return console.error(err.stack)
             f.forEach(category => {
-                readdir(`${__dirname}/${category}`, (err, cmd) => {
+                readdir(`${__dirname}/commands/${category}`, (err, cmd) => {
                     cmd.forEach(cmd => {
-                        const Command = require(`${__dirname}/${category}/${cmd}`)
+                        const Command = require(`${__dirname}/commands/${category}/${cmd}`)
                         const command = new Command(this)
                         this.commands.set(command.config.name, command)
                         command.config.aliases.forEach(alias => this.aliases.set(alias, command.config.name))
@@ -33,10 +33,10 @@ module.exports = class ShiroClient extends Client {
     }
 
     loadEvents() {
-        readdir(__dirname, (err, f) => {
+        readdir(`${__dirname}/events`, (err, f) => {
             if (err) return console.error(err.stack)
             f.forEach(events => {
-                const Event = require(`${__dirname}/${events}`)
+                const Event = require(`${__dirname}/events/${events}`)
                 const event = new Event(this)
                 super.on(events.split(".")[0], (...args) => event.run(...args))
             })
