fce9aaaafa82fe7d87359763a15557240f84af7a
DanielaGC <danielagc676@gmail.com>
2022-05-22 23:15:01 -0300

    fix: Fixing alot of typos

--- src/commands/music/VolumeCommand.js
+++ src/commands/music/VolumeCommand.js
@@ -18,7 +18,7 @@ module.exports = class VolumeCommand extends Command {
       if (!ctx.message.member.voiceState.channelID) return ctx.quote(ctx.locale('commands:dj-module.user-channel-null'))
       if (ctx.message.channel.guild.members.get(ctx.client.user.id).voiceState.channelID && ctx.message.member.voiceState.channelID !== ctx.message.channel.guild.members.get(ctx.client.user.id).voiceState.channelID) return ctx.quote(ctx.locale('commands:dj-module.user-another-channel', { channel: ctx.message.channel.guild.members.get(ctx.client.user.id).voiceState.channelID.name }))
       if (!ctx.args[0]) return ctx.quote(ctx.locale('commands:current', { volume: ctx.client.player.get(ctx.message.guildID).player.state.volume }))
-      if (ctx.args[0] > 100 && ctx.args[0] < 5) return ctx.quote(ctx.locale('commands:volume.max-and-min', { volume: ctx.client.player.get(ctx.message.guildID).player.state.volume }))
+      if (ctx.args[0] > 100 && ctx.args[0] < 5) return ctx.quote(ctx.locale('commands:volume.max-and-min'))
 
       ctx.client.player.get(ctx.message.guildID).player.volume(ctx.args[0])
       ctx.quote(ctx.locale('commands:volume.changed', { volume: ctx.client.player.get(ctx.message.guildID).player.state.volume }))
@@ -28,7 +28,7 @@ module.exports = class VolumeCommand extends Command {
       if (!ctx.message.member.voiceState.channelID) return ctx.quote(ctx.locale('commands:dj-module.user-channel-null'))
       if (ctx.message.channel.guild.members.get(ctx.client.user.id).voiceState.channelID && ctx.message.member.voiceState.channelID !== ctx.message.channel.guild.members.get(ctx.client.user.id).voiceState.channelID) return ctx.quote(ctx.locale('commands:dj-module.user-another-channel', { channel: ctx.message.channel.guild.members.get(ctx.client.user.id).voiceState.channelID.name }))
       if (!ctx.args[0]) return ctx.quote(ctx.locale('commands:current', { volume: ctx.client.player.get(ctx.message.guildID).player.state.volume }))
-      if (ctx.args[0] > 100 && ctx.args[0] < 5) return ctx.quote(ctx.locale('commands:volume.max-and-min', { volume: ctx.client.player.get(ctx.message.guildID).player.state.volume }))
+      if (ctx.args[0] > 100 && ctx.args[0] < 5) return ctx.quote(ctx.locale('commands:volume.max-and-min'))
 
       ctx.client.player.get(ctx.message.guildID).player.volume(ctx.args[0])
       ctx.quote(ctx.locale('commands:volume.changed', { volume: ctx.client.player.get(ctx.message.guildID).player.state.volume }))
